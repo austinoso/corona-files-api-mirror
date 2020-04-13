99bf7636d2da604307a1b12b99d66d87edd755d7
austinoso <osorio_austin@yahoo.com>
2020-04-13 11:11:48 -0700

    added auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :posts
     has_many :votes
-    # has_many :posts, through: :votes, as: :voted_posts
+    has_secure_password
+    validates :username, uniqueness: { case_sensitive: false }
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@ Vote.destroy_all
 Post.destroy_all
 User.destroy_all
 
-user1 = User.create!(username: "aaronminyard")
-user2 = User.create!(username: "orio")
+user1 = User.create!(username: "aaronminyard", password: 'password' )
+user2 = User.create!(username: "orio", password: 'password')
 post1 = Post.create!(title: "U.S. surpasses Italy for most confirmed coronavirus deaths", content: "A day after setting a new high in daily fatalities, the United States on Saturday surpassed Italy for the most confirmed covid-19 deaths in the world, a figure experts have called 'an underestimation.", article_link:"https://www.washingtonpost.com/world/2020/04/11/coronavirus-latest-news/", user: user1)
 vote1 = Vote.create!(user: user2, post: post1, status: true)
 
