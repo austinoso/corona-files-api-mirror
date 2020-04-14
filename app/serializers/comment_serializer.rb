2cb549f714459000eeaeaf04f7ce1c674cc7c1c0
austinoso <osorio_austin@yahoo.com>
2020-04-13 20:20:57 -0700

    added comments

---
+++ app/serializers/comment_serializer.rb
@@ -0,0 +1,5 @@
+class CommentSerializer < ActiveModel::Serializer
+  attributes :id, :content, :user
+  belongs_to :user, serializer: UserSerializer
+
+end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,6 +1,7 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :article_link, :score
-  belongs_to :user, include_nested_associations: true
+  belongs_to :user
+  has_many :comments, include_nested_associations: true
 
 
 end
