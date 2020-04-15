20eebc975dd8eb2c0bc7353974020cdb642ec179
amnyrrd <amnyrrd@gmail.com>
2020-04-15 16:20:09 -0700

    yep

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :content, :user
-  belongs_to :user, serializer: UserSerializer
+  belongs_to :user#, serializer: UserSerializer
 
 end
