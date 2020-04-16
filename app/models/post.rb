5adadb03ba7ac8aa8efe633f60d38c0e8b875628
amnyrrd <amnyrrd@gmail.com>
2020-04-16 11:29:24 -0700

    presented version

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ApplicationRecord
   has_many :votes, dependent: :destroy 
   has_many :comments, dependent: :destroy
 
+  validates :title, :article_link, presence: true
+
+
   def score
     agrees = self.votes.find_all { |vote| vote.status }.count
     not_agrees = self.votes.count - agrees
